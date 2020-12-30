e1dc0576b63a9da2ffba7ff365640bf0e44821f0
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2020-12-30 09:19:59 -0800

    decrease number of workers

--- DARTS_CNN/train_search.py
+++ DARTS_CNN/train_search.py
@@ -23,7 +23,7 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 parser = argparse.ArgumentParser("cifar")
 parser.add_argument('--data', type=str, default='../data', help='location of the data corpus')
-parser.add_argument('--batch_size', type=int, default=20, help='batch size')
+parser.add_argument('--batch_size', type=int, default=64, help='batch size')
 parser.add_argument('--learning_rate', type=float, default=0.025, help='init learning rate')
 parser.add_argument('--learning_rate_min', type=float, default=0.001, help='min learning rate')
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
