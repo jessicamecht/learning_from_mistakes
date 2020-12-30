8e126c46c9c275382f5b3a4156aed6a820d926b0
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2020-12-29 19:31:59 -0800

    add debug statement

--- visual_similarity/visual_similarity.py
+++ visual_similarity/visual_similarity.py
@@ -39,9 +39,9 @@ def visual_validation_similarity(validation_examples, training_examples, model):
         assert (not torch.isnan(validation_examples_embedding.view(256, 2048, 1)).any())
         dot_product = torch.bmm(elem.view(256, 1, 2048), validation_examples_embedding.view(256, 2048, 1))
         dot_products = torch.cat((dot_products, dot_product), dim=1)
-        if torch.isnan(dot_products).any():
-            #print(dot_product)
-            #print(torch.isnan(elem).any(), elem.view(256, 1, 2048), torch.isnan(validation_examples_embedding).any(), validation_examples_embedding.view(256, 2048, 1))
+        #if torch.isnan(dot_products).any():
+        #    #print(dot_product)
+        #    #print(torch.isnan(elem).any(), elem.view(256, 1, 2048), torch.isnan(validation_examples_embedding).any(), validation_examples_embedding.view(256, 2048, 1))
 
     assert(not torch.isnan(dot_products).any())
 
