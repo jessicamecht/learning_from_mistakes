ddeeacace55896c2e7a2d13a697d4770e7924e2c
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-01-02 09:23:02 -0800

    check if path exists

--- dartsLCT/utils.py
+++ dartsLCT/utils.py
@@ -134,7 +134,9 @@ def create_exp_dir(path, scripts_to_save=None):
   print('Experiment dir : {}'.format(path))
 
   if scripts_to_save is not None:
-    os.mkdir(os.path.join(path, 'scripts'), exist_ok=True)
+    if not os.path.exists(os.path.join(path, 'scripts')):
+      os.mkdir(os.path.join(path, 'scripts'))
+
     for script in scripts_to_save:
       dst_file = os.path.join(path, 'scripts', os.path.basename(script))
       shutil.copyfile(script, dst_file)
