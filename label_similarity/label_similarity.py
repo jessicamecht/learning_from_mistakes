ad4b8d6a60ada0320430cd3b019a50d6ae54c7bc
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-01-27 18:59:09 -0800

    fix label similarity

--- label_similarity/label_similarity.py
+++ label_similarity/label_similarity.py
@@ -1,14 +1,26 @@
 import os, sys
+import torch
 sys.path.append('../')
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from weighted_data_loader import loadCIFARData, getWeightedDataLoaders
 
-def measure_label_similarity(training_example_targets, validation_example_targets):
-    '''checks for each training and test label if they are the same'''
-    res = training_example_targets.unsqueeze(1) - validation_example_targets
-    res = (res == 0).float()
-    assert(res.shape[0] == training_example_targets.shape[0] and res.shape[1] == validation_example_targets.shape[0])
-    return res
+def measure_label_similarity(validation_targets, training_targets):
+    '''checks for each training and test label if they are the same
+    :param validation_targets torch of size (number val targets)
+    :param training_targets torch of size (number train targets)
+    :returns torch of size (number training targets, number val targets)'''
+
+    val_targets = validation_targets.reshape(validation_targets.shape[0], 1)
+    train_targets = training_targets.reshape(training_targets.shape[0], 1)
+
+    train_targets = torch.repeat_interleave(train_targets, val_targets.shape[0], dim=1)
+    val_targets = torch.repeat_interleave(val_targets, train_targets.shape[0], dim=1)
+
+    val_train_diff = val_targets.T - train_targets
+    assert(val_train_diff[0][0] == validation_targets[0] - training_targets.T[0] )
+    assert(val_train_diff.shape[0] == training_targets.shape[0] and val_train_diff.shape[1] == validation_targets.shape[0])
+    val_train_equality = (val_train_diff == 0).float()
+    return val_train_equality
 
 
 if __name__ == "__main__":
@@ -16,6 +28,6 @@ if __name__ == "__main__":
     train_loader, val_loader, test_loader = getWeightedDataLoaders(train_data, val_data, test_data, batch_size=10)
     train_imgs, train_targets, train_weights = next(iter(train_loader))
     val_imgs, val_targets, val_weights = next(iter(val_loader))
-    print(measure_label_similarity(train_targets, val_targets).shape)
+    print(measure_label_similarity(val_targets, train_targets).shape)
 
 
