21860fd41426aa22f27bc45f31877db856d16ec3
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-01-27 17:50:31 -0800

    remove comment

--- label_similarity/label_similarity.py
+++ label_similarity/label_similarity.py
@@ -7,7 +7,6 @@ def measure_label_similarity(training_example_targets, validation_example_target
     '''checks for each training and test label if they are the same'''
     res = training_example_targets.unsqueeze(1) - validation_example_targets
     res = (res == 0).float()
-    #dimension check
     assert(res.shape[0] == training_example_targets.shape[0] and res.shape[1] == validation_example_targets.shape[0])
     return res
 
@@ -17,7 +16,6 @@ if __name__ == "__main__":
     train_loader, val_loader, test_loader = getWeightedDataLoaders(train_data, val_data, test_data, batch_size=10)
     train_imgs, train_targets, train_weights = next(iter(train_loader))
     val_imgs, val_targets, val_weights = next(iter(val_loader))
-
     print(measure_label_similarity(train_targets, val_targets).shape)
 
 
