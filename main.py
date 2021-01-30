65dbdb962e131148f9096269e3a2a16845b6e695
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-01-30 13:01:27 -0800

    change epochs

--- main.py
+++ main.py
@@ -23,11 +23,11 @@ def main():
                    "reduce_concat=range(2, 6))"
     w_config = load_config('weight_samples/config.yml')
     in_size = train_data[0][0].shape[1]
-    #augment.main(in_size, train_queue, val_queue, genotype)
+    augment.main(in_size, train_queue, val_queue, genotype)
 
     # Use validation performance to re-weight each training example with three scores
     # for each training sample and update them in instance_weights.npy
-    #update_similarity_weights.calculate_similarity_weights(train_data, train_queue, val_queue, w_config)
+    update_similarity_weights.calculate_similarity_weights(train_data, train_queue, val_queue, w_config)
 
     # Second Stage: based on the calculated weights for each training instance, calculates a second
     # set of weights given the DARTS architecture by minimizing weighted training loss
