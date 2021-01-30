ab0dd35357c67a957e3febe8b44d1dbcc0f8a700
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-01-30 13:52:42 -0800

    debug weighted loss

--- loss.py
+++ loss.py
@@ -3,7 +3,7 @@ import numpy as np
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-def calculate_weighted_loss(logits, target, criterion, weights):
+def calculate_weighted_loss(logits, target, criterion, weights, reduce=True):
     '''calculates the weighted loss for each input example, and then averages everything in the end
     :param input torch tensor of size (number of examples, feature_dimension_1, ...)
     :param target torch tensor of size (number of examples,)
@@ -14,5 +14,5 @@ def calculate_weighted_loss(logits, target, criterion, weights):
     weights = weights.cpu()
     weights = torch.tensor(np.array(weights).astype(float)).to(device)
     weighted_loss_individual = loss.float() * weights.float()
-    loss = torch.mean(weighted_loss_individual)
+    loss = torch.mean(weighted_loss_individual) if reduce else weighted_loss_individual
     return loss
