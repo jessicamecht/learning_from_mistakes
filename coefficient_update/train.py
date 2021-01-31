48adfc6d6bbd1632029b0ba94a7896f596b481c2
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-01-30 17:42:48 -0800

    debug coeff learning

--- coefficient_update/train.py
+++ coefficient_update/train.py
@@ -55,7 +55,7 @@ def train(train_queue, val_queue, pred_model, learning_rate=0.01, epochs=100):
                 optimizer.step()
 
                 val_loss = criterion(val_logits, val_target)
-                print('Update Coefficients: Epoch {}, Train loss {}, Val loss {}'.format(epoch, loss.item(), val_loss))
+                print('Update Coefficients: Epoch {}, Train loss {}, Val loss {}'.format(epoch, loss.item(), val_loss.item()))
                 script_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
                 torch.save(model.state_dict(), script_dir + '/coefficient_update/weights/r.pt')
 
