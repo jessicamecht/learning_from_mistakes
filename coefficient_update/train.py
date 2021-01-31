0e23fdd66c632f185c895ba3dce36e1ba4018ee7
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-01-30 17:38:59 -0800

    debug coeff learning

--- coefficient_update/train.py
+++ coefficient_update/train.py
@@ -26,7 +26,7 @@ def train(train_queue, val_queue, pred_model, learning_rate=0.01, epochs=100):
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
     pred_model.eval()
     script_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    torch.save(model.state_dict(), script_dir + 'weights/r.pt')
+    torch.save(model.state_dict(), script_dir + '/coefficient_update/weights/r.pt')
 
     for epoch in range(epochs):
         for step, loader in enumerate(val_queue):
