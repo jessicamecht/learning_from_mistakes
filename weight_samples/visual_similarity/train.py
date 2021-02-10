f8d8ea456357ecc0b9e2ebd0e55dd0081ae9b15c
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-02-10 12:27:33 -0800

    debug visual embedding learning

--- weight_samples/visual_similarity/train.py
+++ weight_samples/visual_similarity/train.py
@@ -18,8 +18,8 @@ def train(train_loader, val_loader, learning_rate=0.001, epochs=100):
     criterion = criterion.to(device)
     optimizer = optim.Adam(model.fc.parameters(), lr=learning_rate)
 
-
     train_losses, val_losses = [], []
+    train_accuracy, val_accuracy = [],[]
     for epoch in range(epochs):
         running_loss = 0
         for steps, (inputs, labels, weights) in enumerate(train_loader):
@@ -32,7 +32,7 @@ def train(train_loader, val_loader, learning_rate=0.001, epochs=100):
             optimizer.step()
             running_loss += loss.item()
 
-            if steps % 50 == 0:
+            if steps % 10 == 0:
                 model.eval()
                 with torch.no_grad():
                     val_loss = 0
@@ -47,15 +47,13 @@ def train(train_loader, val_loader, learning_rate=0.001, epochs=100):
                         top_p, top_class = ps.topk(1, dim=1)
                         equals = top_class == labels.view(*top_class.shape)
                         accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
-                    val_losses.append(val_loss / len(val_loader))
-            train_losses.append(running_loss / len(train_loader))
+                val_losses.append(val_loss / len(val_loader))
+                train_losses.append(running_loss / len(train_loader))
 
-        print(f"Epoch {epoch + 1}/{epochs}.. "
-              f"Train loss: {running_loss / 50:.3f}.. "
-              f"Validation loss: {val_loss / len(val_loader):.3f}.. "
-              f"Validation accuracy: {accuracy / len(val_loader):.3f}")
-        running_loss = 0
-        model.train()
+                print(f"Epoch {epoch + 1}/{epochs}.. "
+                    f"Train loss: {running_loss / 10:.3f}.. "
+                    f"Validation loss: {val_loss / len(val_loader):.3f}.. "
+                    f"Validation accuracy: {accuracy / len(val_loader):.3f}")
 
     script_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     torch.save(model.state_dict(), script_dir + '/state_dicts/weighted_resnet_model.pt')
