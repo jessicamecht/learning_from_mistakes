a59dd82cc75b5f497c6bf14796b021fc101324d5
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-02-10 11:56:32 -0800

    debug visual embedding learning

--- weight_samples/visual_similarity/train.py
+++ weight_samples/visual_similarity/train.py
@@ -4,7 +4,6 @@ from torch import optim
 import os, sys
 sys.path.append('../')
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from data_loader.weighted_data_loader import loadCIFARData, getWeightedDataLoaders
 import weight_samples.visual_similarity.resnet_model as resnet_model
 from loss import calculate_weighted_loss
 
@@ -12,17 +11,17 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 def train(train_loader, val_loader, learning_rate=0.001, epochs=100):
-    model = resnet_model.resnet50(pretrained=False)
+    model = resnet_model.resnet50(pretrained=True)
     model = model.to(device)
 
     criterion = nn.CrossEntropyLoss(reduction='none')
     criterion = criterion.to(device)
     optimizer = optim.Adam(model.fc.parameters(), lr=learning_rate)
 
-    running_loss = 0
 
     train_losses, val_losses = [], []
     for epoch in range(epochs):
+        running_loss = 0
         for steps, (inputs, labels, weights) in enumerate(train_loader):
             model.train()
             inputs, labels, weights = inputs.to(device), labels.to(device), weights.to(device)
@@ -34,22 +33,23 @@ def train(train_loader, val_loader, learning_rate=0.001, epochs=100):
             running_loss += loss.item()
 
             if steps % 50 == 0:
-                val_loss = 0
-                accuracy = 0
                 model.eval()
                 with torch.no_grad():
+                    val_loss = 0
+                    accuracy = 0
                     for inputs, labels, weights in val_loader:
                         inputs, labels, weights = inputs.to(device), labels.to(device), weights.to(device)
-                    logps = model.forward(inputs)
-                    batch_loss = torch.mean(criterion(logps, labels))
-                    val_loss += batch_loss.item()
+                        logps = model.forward(inputs)
+                        batch_loss = torch.mean(criterion(logps, labels))
+                        val_loss += batch_loss.item()
 
-                    ps = torch.exp(logps)
-                    top_p, top_class = ps.topk(1, dim=1)
-                    equals = top_class == labels.view(*top_class.shape)
-                accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
-        train_losses.append(running_loss / len(train_loader))
-        val_losses.append(val_loss / len(val_loader))
+                        ps = torch.exp(logps)
+                        top_p, top_class = ps.topk(1, dim=1)
+                        equals = top_class == labels.view(*top_class.shape)
+                        accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+                    val_losses.append(val_loss / len(val_loader))
+            train_losses.append(running_loss / len(train_loader))
+
         print(f"Epoch {epoch + 1}/{epochs}.. "
               f"Train loss: {running_loss / 50:.3f}.. "
               f"Validation loss: {val_loss / len(val_loader):.3f}.. "
