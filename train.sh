58572bf82edc96b93e3f2949327213af8702d4ef
Jessica Echterhoff <j.m.echt@gmail.com>
2021-02-10 18:29:15 +0000

    add train and environment file

---
+++ train.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+cat train.sh
+cat environment.yaml
+nvidia-smi
+#conda update -n base -c defaults conda
+conda env create -f environment.yaml
+#conda init bash
+#exec bash
+source activate lfm
+nvidia-smi
+python main.py
+
