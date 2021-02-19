7b8f8211b4948ca299678b983e5064762ce402e8
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-02-19 15:35:03 -0800

    no message

--- ptdarts/search.py
+++ ptdarts/search.py
@@ -70,7 +70,7 @@ def main(train_loader, valid_loader, config_path, writer):
 
     #Init coefficient vector r update model
     inputDim = next(iter(valid_loader))[0].shape[0]
-    coefficient_update_model = LinearRegression(inputDim, 1, nn.Criterion())
+    coefficient_update_model = LinearRegression(inputDim, 1, nn.CrossEntropyLoss())
     coefficient_update_optimizer = torch.optim.Adam(coefficient_update_model.parameters(), config.alpha_lr, betas=(0.5, 0.999),
                                                 weight_decay=config.alpha_weight_decay)
 
