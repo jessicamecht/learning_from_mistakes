ea950f2b601e5bc9becedbaffb0eac967647374e
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-02-24 07:21:24 -0800

    test

--- ptdarts/architect.py
+++ ptdarts/architect.py
@@ -139,6 +139,8 @@ class Architect():
 
         for i, (w, vw, g) in enumerate(zip(self.net.weights(), self.v_net.weights(), gradients)):
             m = w_optim.state[w].get('momentum_buffer', 0.) * self.w_momentum
+            if i == 0:
+                print(vw, '1')
             vw.data = torch.clone(w - xi * (m + g + self.w_weight_decay*w))
             if i == 0:
                 print(vw)
