32ccf5d4dde243e5d098c322ebdf310dc314c137
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-02-26 09:43:22 -0800

    meta-learn coeff and vis encoder

--- ptdarts/architect.py
+++ ptdarts/architect.py
@@ -30,7 +30,7 @@ class Architect():
         with higher.innerloop_ctx(model, optimizer) as (fmodel, foptimizer):
             logits = fmodel(input)
             weights = self.calc_weights(input, target, input_val, target_val, model, coefficient_vector, visual_encoder)
-            loss = weights * F.cross_entropy(logits, target, reduction='none')
+            loss = torch.mean(weights * F.cross_entropy(logits, target, reduction='none'))
             foptimizer.step(loss)
 
             logits = fmodel(input)
