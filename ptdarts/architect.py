6aca25eb9f387e408aef2330345f19aa96f5f72d
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-02-26 11:01:40 -0800

    fix bugs

--- ptdarts/architect.py
+++ ptdarts/architect.py
@@ -39,10 +39,12 @@ class Architect():
             visual_encoder_gradients = torch.autograd.grad(val_loss, visual_encoder.parameters())#equivalent to backward but only for given parameters
             coeff_vector_gradients = coeff_vector_gradients.detach()
             with torch.no_grad():
-                print(self.coefficient_vector.shape, self.w_weight_decay, coeff_vector_gradients.shape)
-                self.coefficient_vector = self.coefficient_vector - self.w_weight_decay * coeff_vector_gradients
+
                 for p, p_new in zip(self.visual_encoder_model.parameters(), visual_encoder_gradients):
                     p.copy_(p-self.w_weight_decay*p_new) #TODO momentum and stuff
+
+                print(self.coefficient_vector.shape, self.w_weight_decay, coeff_vector_gradients)
+                self.coefficient_vector = self.coefficient_vector - self.w_weight_decay * coeff_vector_gradients
 
     def calc_instance_weights(self, input_train, target_train, input_val, target_val, model, coefficient, visual_encoder):
         val_logits = model(input_val)
