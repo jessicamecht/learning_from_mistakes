d9f1ecdb276940adf539d39054546b903581f748
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-02-28 07:53:21 -0800

    wrap params

--- ptdarts/search.py
+++ ptdarts/search.py
@@ -79,7 +79,7 @@ def main():
     best_top1 = 0.
     for epoch in range(config.epochs):
         lr_scheduler.step()
-        lr = lr_scheduler.get_last_lr()
+        lr = lr_scheduler.get_lr()[0]
 
         model.print_alphas(logger)
         architect.print_coefficients(logger)
