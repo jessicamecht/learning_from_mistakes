da5b8f7b697e3cd9ca89b7e19fe01fa5327c35bb
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-03-11 22:11:52 -0800

    change # worker

--- ptdarts/architect.py
+++ ptdarts/architect.py
@@ -15,7 +15,7 @@ def debug_memory():
     tensors = collections.Counter((str(o.device), o.dtype, tuple(o.shape))
                                   for o in gc.get_objects()
                                   if torch.is_tensor(o))
-    for line in sorted(tensors.items()):
+    for line in tensors.items():
         print('{}\t{}'.format(*line))
 
 
