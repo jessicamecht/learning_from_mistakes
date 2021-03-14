3bb0130b223e2e7950db18043541aec55516ac35
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-03-13 16:02:29 -0800

    change # worker

--- ptdarts/architect.py
+++ ptdarts/architect.py
@@ -247,7 +247,8 @@ def meta_learn(model, optimizer, input, target, input_val, target_val, coefficie
         coeff_vector_gradients = torch.autograd.grad(meta_val_loss, coefficient_vector, retain_graph=True)
         coeff_vector_gradients = coeff_vector_gradients[0].detach()
         visual_encoder_gradients = torch.autograd.grad(meta_val_loss,
-                                                           visual_encoder.parameters()).detach()  # equivalent to backward for given parameters
+                                                           visual_encoder.parameters())
+        visual_encoder_gradients = (visual_encoder_gradients[0].detach(), visual_encoder_gradients[1].detach())# equivalent to backward for given parameters
     print('memory_allocatedtlast', torch.cuda.memory_allocated() / 1e9, 'memory_reserved',
               torch.cuda.memory_reserved() / 1e9)
 
