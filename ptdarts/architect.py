629b5bca405bcf07c9a49105dad7cd6bcdb8b0ec
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-03-15 11:43:16 -0700

    change # worker

--- ptdarts/architect.py
+++ ptdarts/architect.py
@@ -57,7 +57,7 @@ class Architect():
         new_vis = copy.deepcopy(self.visual_encoder_model)
         net_copy = copy.deepcopy(self.net)
         w_optim_copy = copy.deepcopy(w_optim)
-        visual_encoder_gradients, coeff_vector_gradients = meta_learn(net_copy, w_optim, trn_X, trn_y, val_X, val_y, new_coeff, new_vis)
+        visual_encoder_gradients, coeff_vector_gradients = meta_learn(net_copy, w_optim_copy, trn_X, trn_y, val_X, val_y, new_coeff, new_vis)
         update_gradients(visual_encoder_gradients, coeff_vector_gradients, self.visual_encoder_model, self.coefficient_vector)
         del new_coeff, new_vis, net_copy, w_optim_copy
         gc.collect()
