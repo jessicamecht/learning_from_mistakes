5f27ea7658afed20497cbf7b5a3537d4faa8694d
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-03-24 15:00:11 -0700

    set flags to false

--- ptdarts/weight_samples/sample_weights.py
+++ ptdarts/weight_samples/sample_weights.py
@@ -47,7 +47,7 @@ def calc_instance_weights(input_train, target_train, input_val, target_val, val_
     crit = nn.CrossEntropyLoss(reduction='none')
     #preds, _ = torch.max(val_logits,1)
     #print(preds.shape, target_val.shape)
-    predictive_performance = crit(preds, target_val)
+    predictive_performance = crit(val_logits, target_val)
     vis_similarity = visual_validation_similarity(visual_encoder, input_val, input_train)
     label_similarity = measure_label_similarity(target_val, target_train)
     weights = sample_weights(predictive_performance, vis_similarity, label_similarity, coefficient)
