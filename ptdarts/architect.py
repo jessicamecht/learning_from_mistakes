90b06dc8d62eb3fe55ffccfe81e1b82a46036139
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-03-24 17:14:01 -0700

    set flags to false

--- ptdarts/architect.py
+++ ptdarts/architect.py
@@ -218,7 +218,7 @@ def meta_learn(model, optimizer, input, target, input_val, target_val, coefficie
             print('memory_allocatedt2', torch.cuda.memory_allocated() / 1e9, 'memory_reserved',
                   torch.cuda.memory_reserved() / 1e9)
 
-
+            print(input.requires_grad, target.requires_grad, target_val.requires_grad, input_val.requires_grad)
             weights = calc_instance_weights(input, target, input_val, target_val, logits_val, coefficient_vector, visual_encoder)
             weighted_training_loss = torch.mean(weights * F.cross_entropy(logits, target, reduction='none'))
             foptimizer.step(weighted_training_loss, fmodel.parameters())  # replaces gradients with respect to model weights -> w2
