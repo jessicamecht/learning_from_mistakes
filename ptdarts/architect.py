58fb19de03211e065f58b8471821d55f0ca990de
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-03-24 18:04:37 -0700

    set flags to false

--- ptdarts/architect.py
+++ ptdarts/architect.py
@@ -54,12 +54,12 @@ class Architect():
         print('memory_allocated2', torch.cuda.memory_allocated() / 1e9, 'memory_reserved',
               torch.cuda.memory_reserved() / 1e9)
 
-        new_coeff = copy.deepcopy(self.coefficient_vector)
-        new_vis = copy.deepcopy(self.visual_encoder_model)
-        net_copy = copy.deepcopy(self.net)
-        w_optim_copy = torch.optim.SGD(net_copy.parameters(), 0.01)
-        trn_X_copy, trn_y_copy, val_X_copy, val_y_copy = copy.deepcopy(trn_X), copy.deepcopy(trn_y), copy.deepcopy(val_X), copy.deepcopy(val_y)
-
+        new_coeff = self.coefficient_vector #copy.deepcopy(self.coefficient_vector)
+        new_vis = self.visual_encoder_model# copy.deepcopy(self.visual_encoder_model)
+        net_copy = self.net#copy.deepcopy(self.net)
+        w_optim_copy = w_optim#torch.optim.SGD(net_copy.parameters(), 0.01)
+        #trn_X_copy, trn_y_copy, val_X_copy, val_y_copy = copy.deepcopy(trn_X), copy.deepcopy(trn_y), copy.deepcopy(val_X), copy.deepcopy(val_y)
+        trn_X_copy, trn_y_copy, val_X_copy, val_y_copy = trn_X, trn_y, val_X, val_y
         print('memory_allocatedtest', torch.cuda.memory_allocated() / 1e9, 'memory_reserved',
               torch.cuda.memory_reserved() / 1e9)
         visual_encoder_gradients, coeff_vector_gradients = meta_learn(net_copy, w_optim_copy, trn_X_copy, trn_y_copy, val_X_copy, val_y_copy, new_coeff, new_vis)
