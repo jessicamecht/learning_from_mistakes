c21a1f0e45e1f5c24f98e4574c2aa14385d35a3e
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-03-26 11:05:30 -0700

    fix higher issue

--- ptdarts/test.py
+++ ptdarts/test.py
@@ -103,7 +103,9 @@ if __name__ == "__main__":
     visual_encoder_model = Resnet_Encoder(nn.CrossEntropyLoss())
     visual_encoder_model = visual_encoder_model.to(device)
     w_optim = torch.optim.SGD(list(model.parameters()), 0.01)
+    GPUtil.showUtilization()
     a,b = meta_learn_test(model, w_optim, inp, targ, inp_val, targ_val, coefficient_vector, visual_encoder_model)
+    GPUtil.showUtilization()
     count_tensors(app="5")
     GPUtil.showUtilization()
     del inp, inp_val, targ, targ_val, model, visual_encoder_model, coefficient_vector, a, b, w_optim, test_data, test_loader, val_data, valid_loader, root, train_data, train_size
