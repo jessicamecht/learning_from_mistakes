5e496cc0aaf90d88626ef96a001a2c8fa2f87283
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-03-26 11:11:57 -0700

    fix higher issue

--- ptdarts/test.py
+++ ptdarts/test.py
@@ -107,7 +107,7 @@ if __name__ == "__main__":
     #a,b = meta_learn_test(model, w_optim, inp, targ, inp_val, targ_val, coefficient_vector, visual_encoder_model)
    # GPUtil.showUtilization()
     count_tensors(app="5")
-    del inp, inp_val, targ, targ_val, model, visual_encoder_model, coefficient_vector, a, b, w_optim, test_data, test_loader, val_data, valid_loader, root, train_data, train_size
+    del inp, inp_val, targ, targ_val, model, visual_encoder_model, coefficient_vector, w_optim, test_data, test_loader, val_data, valid_loader, root, train_data, train_size
     gc.collect()
     torch.cuda.empty_cache()
 
